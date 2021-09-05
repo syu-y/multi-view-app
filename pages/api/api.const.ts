622479c398e65446887fa2f44d546246d6c62f3e
syu-y <syu@12580526@gmail.com>
2021-09-05 17:27:30 +0900

    定数ファイル追加

---
+++ pages/api/api.const.ts
@@ -0,0 +1,17 @@
+export const OFFICIAL_MEMBER_PAGE_HOLOLIVE =
+  'https://hololive.hololivepro.com/talents/';
+export const OFFICIAL_STORE_PAGE_NIJISANJI =
+  'https://shop.nijisanji.jp/s/niji/search/artist?ima=2110';
+export const OFFICIAL_STORE_BASE_URL_NIJISANJI = 'https://shop.nijisanji.jp';
+
+export const YOUTUBE_CHANNEL_BASE_URL = [
+  'https://www.youtube.com/channel/',
+  'https://youtube.com/channel/',
+];
+export const YOUTUBE_RSS_URL = `https://www.youtube.com/feeds/videos.xml?channel_id=`;
+export const TWITTER_BASE_URL = 'https://twitter.com/';
+export const TAG = {
+  hololive: 'ホロライブ',
+  nijisanji: 'にじさんじ',
+  other: 'その他・個人',
+};
